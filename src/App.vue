f1d2390c09585e6bc328bfdde04ac204397bf14e
Walker Griffin <38767282+WalkerJG33@users.noreply.github.com>
2018-10-04 11:28:54 -0500

    added authentication to client (#4)

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,48 @@
    
     <SearchBar/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <button @click="testing">testing</button>
+    <button
+    v-if="!authenticated" 
+    @click="login()">
+    Log-in
+    </button>
+    <button
+    v-if="authenticated"
+    @click="logout()">
+    Log Out
+    </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import SearchBar from './components/searchBar.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import SearchBar from './components/searchBar.vue';
+import AuthService  from './auth/authServices.js';
+
+const auth = new AuthService();
+const { login, logout, authenticated, authNotifier} = auth;
 
 export default {
   name: 'app',
+   data () {
+    authNotifier.on('authChange', authState => {
+      this.authenticated = authState.authenticated
+    })
+    // console.log('Auth in app.vue', auth);
+    auth.handleAuthentication();
+    
+    return {
+      auth,
+      authenticated
+    }
+  },
   components: {
     HelloWorld,
     SearchBar
   },
   methods: {
-    testing: function () {
-      console.log(`Is this working? ${SearchBar.name}`)
-    }
+    login,
+    logout
   }
 }
 </script>
