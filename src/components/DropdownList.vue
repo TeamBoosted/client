86136107309bd2ad51383807f49d42e83e5abad5
Hunter Leeves <35352754+foyy@users.noreply.github.com>
2018-10-04 13:53:09 -0500

    Feature/mvp front end (#5)
    * working pseudo-routes
    
    * added capturing events from child comonents

--- src/components/DropdownList.vue
+++ src/components/DropdownList.vue
@@ -1,8 +1,9 @@
-
-
 <template>
   <ul>
-    <li @click="searchForMovie">{{movie.title}}</li>
+    <li class="title" @click="searchForMovie(); toggleSearchResults()" >{{movie.title}}</li>
+      <ul>
+        <li>{{movie.overview}}</li>
+      </ul>
     </ul>
 </template>
 
@@ -15,19 +16,26 @@ export default {
     searchForMovie: function () {
       //axios request here
       console.log(`I'm saving for`,this.movie.title)
-     
     },
     saveToDatabase: function () {
       console.log(`I'm searching the database!!!`)
-       // axios.get(`api/info/movie/${this.input}`)
+      // axios.get(`api/info/movie/${this.input}`)
       //   .then(function (response) {
       //     console.log(response);
       //   })
       //   .catch(function (error) {
       //     console.log(error);
       // });
+    },
+    toggleSearchResults: function () {
+      // console.log('hey man!')
+      this.$emit('toggleSearchResults', this.movie.title)
     }
 
   }
 }
 </script>
+
+<style scoped>
+  
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <h4>{{tag}}</h4>
+  </div>
+</template>
+
+
+
+<script>
+  export default {
+    data () {
+      return {
+        title: 'Welcome to BetterArt!',
+        tag: 'A better way to find your favorite things'
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+  h1 {
+    color:lightslategray;
+    text-align: center;
+  }
+  h4 {
+    color:lightsteelblue;
+    text-align: center;
+  }
+</style>
