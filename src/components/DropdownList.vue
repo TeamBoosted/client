4af451ade44a1feb54bfbbc931b9d799260451b4
Daniel Kang <40406108+danielkang674@users.noreply.github.com>
2018-10-06 15:49:19 -0500

    Integration2 (#16)
    * Take off proxy
    
    * integration2

--- src/components/DropdownList.vue
+++ src/components/DropdownList.vue
@@ -1,37 +1,34 @@
 <template>
   <ul>
-    <li class="title" @click="toggleSearchResults(); getRecs(movie); saveToDatabase();">{{movie.title}}</li>
+    <li class="title" @click="toggleSearchResults(); getRecs(movie); saveToDatabase(movie);">{{movie.title}}</li>
       <ul>
-        <img :src="img + movie.image" class='poster' @click="toggleSearchResults(); saveToDatabase(); getRecs(movie);">
+        <img :src="img + movie.image" class='poster' @click="toggleSearchResults(); saveToDatabase(movie); getRecs(movie);">
         <li>{{movie.synopsis}}</li>
       </ul>
     </ul>
 </template>
 
 <script>
-import {get, post} from "axios";
+import { get, post } from "axios";
 export default {
   name: "DropdownList",
   props: ["movie", "saveToDatabase", "getRecs"],
-  data () {
+  data() {
     return {
       img: `https://image.tmdb.org/t/p/w600_and_h900_bestv2`
-    }
+    };
   },
   methods: {
-    
     toggleSearchResults: function() {
       this.$emit("toggleSearchResults", this.movie.title);
     }
-
- 
   }
 };
 </script>
 
 <style scoped>
-  .poster{
-    max-width: 25%;
-    max-height: 25%;
-  }
+.poster {
+  max-width: 25%;
+  max-height: 25%;
+}
 </style>
