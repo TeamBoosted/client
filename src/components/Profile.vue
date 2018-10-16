3e5c8741b4024f4a4a7d144df9c4e821e3891b07
Hunter Leeves <hunterleeves@gmail.com>
2018-10-16 12:27:46 -0500

    working watson graph, integrated analysis,

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,23 +1,38 @@
 <template>
   <div>
-    <p>Welcome to your tailored profile! </p>
-    <p>Here you can find your favorite works of art, as well as those you found interestings</p>
-    <p>We also offer a personality analysis based off of your favorite works of art!</p>
+    <template v-if="this.showChart === false">
+      <p>Welcome to your tailored profile! </p>
+      <p>Here you can find your favorite works of art, as well as those you found interestings</p>
+      <p>We also offer a personality analysis based off of your favorite works of art!</p>
+    </template>
+
     <a 
       class="button-is-primary"
       @click="getPersonality"
     >
-    Get Personality
+    Get Your Artistic Personality
     </a>
-    <template v-if="showChart">
-      <personality-chart 
-      :data="this.chartData" 
-      :chart-id="personality"
-      :options="{responsive: false, maintainAspectRatio: false}"
-      :width="800"
-      :height="600"
-      />
-    </template>
+
+    <div class="columns">
+      <div class="column is-full">
+        <template v-if="showChart" >
+          <personality-chart 
+          :data="this.chartData" 
+          :options="{responsive: true, maintainAspectRatio: false,}"
+          :width="800"
+          :height="800"
+          
+          />
+        </template>
+      </div>
+      
+      
+    </div>
+    <div v-if="this.showChart">
+      <a @click="getUserBreakdown" >Get Analysis!</a>
+    </div>
+
+    
     
   </div>
 </template>
@@ -38,7 +53,8 @@ export default {
           '""Ruthless silver miner, turned oil prospector, Daniel Plainview moves to oil-rich California. Using his adopted son to project a trustworthy, family-man image, Plainview cons local landowners into selling him their valuable properties for a pittance. However, local preacher Eli Sunday suspects Plainviews motives and intentions, starting a slow-burning feud that threatens both their lives."Jeffrey "The Dude" Lebowski, a Los Angeles slacker who only wants to bowl and drink white Russians, is mistaken for another Jeffrey Lebowski, a wheelchair-bound millionaire, and finds himself dragged into a strange series of events involving nihilists, adult film producers, ferrets, errant toes, and large sums of money."Three detectives in the corrupt and brutal L.A. police force of the 1950s use differing methods to uncover a conspiracy behind the shotgun slayings of the patrons at an all-night diner.""'
       },
       showChart: false,
-      chartData: {}
+      chartData: {},
+      personalityInfo: {}
     };
   },
   methods: {
@@ -49,26 +65,46 @@ export default {
         "i am in the watson response in client, here is watsonResponse",
         watsonResponse
       );
+      let personalityNameArray = [];
+      let personalityDataArray = [];
+      let personalityInfo = {};
+      watsonResponse.data.personality.forEach(personality => {
+        personalityNameArray.push(personality.name),
+          personalityDataArray.push(Math.floor(personality.percentile * 100));
+        personalityInfo[personality.name] = Math.floor(
+          personality.percentile * 100
+        );
+      });
+
+      this.personalityInfo = personalityInfo;
       this.showChart = !this.showChart;
       this.chartData = {
-        labels: ["boooom", "watson"],
+        labels: personalityNameArray,
         datasets: [
           {
             label: "GitHub Commits",
-            backgroundColor: "#f87979",
-            data: [40, 20]
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.6)",
+              "rgba(54, 162, 235, 0.6)",
+              "rgba(255, 206, 86, 0.6)",
+              "rgba(75, 192, 192, 0.6)",
+              "rgba(153, 102, 255, 0.6)",
+              "rgba(255, 159, 64, 0.6)"
+            ],
+            data: personalityDataArray
           }
         ]
       };
+    },
+    getUserBreakdown: function() {
+      // I have the obj that shows the relationships. I will be able to provide a breakdown and recommend 1-2 works per medium.
     }
   }
 };
 </script>
 
 <style >
-#personality {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
+.columns {
+  align-content: center;
 }
 </style>
