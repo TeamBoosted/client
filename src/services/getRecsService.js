953f69be40d22888c3a49deff371cce464ea5f58
WalkerJG33 <wgriffinj@gmail.com>
2018-10-17 16:05:47 -0500

    updated getThreeRec service

--- src/services/getRecsService.js
+++ src/services/getRecsService.js
@@ -15,19 +15,31 @@ const getRecsService = async (medium) => {
 };
 
 export const getRecsByGenreService = async (medium) => {
-  const { genre_id } = medium;
+  const { genre_id, moviedb_id } = medium;
   try {
-    let movie, tv, book;
+    const key = moviedb_id
+    let movie, tv, book, value, save;
+    const data = {
+      movie: medium,
+      user: localStorage.id_token
+    }
+    save = await axios.post('http://localhost:80/api/db/addMedium', { data });
+    console.log('I\'m saving now', save); 
     movie = await axios.get(`http://localhost:80/api/rec/movies/genre/${genre_id[0]}`);
     tv = await axios.get(`http://localhost:80/api/rec/tv/genre/${genre_id[0]}`);
-    book = await axios.get(`http://localhost:80/api/db/getBookRecsByGenre/${genre_id[0]}`)
-    return [...movie.data, ...tv.data, ...book.data];
+    book = await axios.get(`http://localhost:80/api/db/getBookRecsByGenre/${genre_id[0]}`);
+    value = [...movie.data, ...tv.data, ...book.data];
+    // const params = {
+    //   key,
+    //   value
+    // }
+    axios.post('http://localhost:80/api/redis/caching', { key, value });
+    
+    // saved = await axios.post(``)
+    return value;
   } catch (err) {
-    throw (err);
+    console.log(err);
   }
 }
 
 export default getRecsService
-// export { getRecsService, getRecsByGenreService };
-// export getRecsByGenreService;
-//`https://api.themoviedb.org/3/tv/${tv_id}/recommendations`;
