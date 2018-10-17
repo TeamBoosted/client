8f9f185d860817d4b137174998dcbc5341a0da30
Daniel Kang <danielkang674@gmail.com>
2018-10-17 16:06:36 -0500

    Books bugs

--- src/services/getRecsService.js
+++ src/services/getRecsService.js
@@ -8,7 +8,7 @@ const getRecsService = async (medium) => {
     } else {
       response = await axios.get(`http://localhost:80/api/rec/tv/${medium.moviedb_id}`)
     }
-    console.log('Here is the recc:',response)
+    console.log('Here is the recc:', response)
     return response;
   } catch (err) {
     console.log(err);
@@ -19,10 +19,20 @@ export const getRecsByGenreService = async (medium) => {
   const { genre_id } = medium;
   try {
     let movie, tv, book;
-    movie = await axios.get(`http://localhost:80/api/rec/movies/genre/${genre_id[0]}`);
-    tv = await axios.get(`http://localhost:80/api/rec/tv/genre/${genre_id[0]}`);
-    book = await axios.get(`http://localhost:80/api/db/getBookRecsByGenre/${genre_id[0]}`)
-    return [...movie.data, ...tv.data, ...book.data];
+    if (medium.type === "movie" || medium.type === "tv") {
+      movie = await axios.get(`http://localhost:80/api/rec/movies/genre/${genre_id[0]}`);
+      tv = await axios.get(`http://localhost:80/api/rec/tv/genre/${genre_id[0]}`);
+      book = await axios.get(`http://localhost:80/api/db/getBookRecsByGenre/${genre_id[0]}`);
+    } else if (medium.type === "book") {
+      movie = await axios.get(`http://localhost:80/api/rec/movies/genre/${medium.book_genre.genreId}`);
+      tv = await axios.get(`http://localhost:80/api/rec/tv/genre/${medium.book_genre.genreId}`);
+      book = await axios.get(`http://localhost:80/api/db/getBookRecsByGenre/${medium.book_genre.genreId}`);
+    }
+    let arr = [...movie.data, ...tv.data];
+    if (book !== null) {
+      arr.concat(book.data);
+    }
+    return arr;
   } catch (err) {
     throw (err);
   }
