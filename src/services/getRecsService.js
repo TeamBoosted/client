b47d44f0ff2bd2d09d693ac20e51a880884e3361
Walker Griffin <38767282+WalkerJG33@users.noreply.github.com>
2018-10-17 17:32:08 -0500

    Feature/more redis (#50)
    * Thumbs up generates robust recommendations
    
    * Removed duplicates
    
    * did this work?
    
    * staging changes for updating
    
    * updated getThreeRec service
    
    * fixed adding to user on thumbs up

--- src/services/getRecsService.js
+++ src/services/getRecsService.js
@@ -8,7 +8,6 @@ const getRecsService = async (medium) => {
     } else {
       response = await axios.get(`http://localhost:80/api/rec/tv/${medium.moviedb_id}`)
     }
-    console.log('Here is the recc:', response)
     return response;
   } catch (err) {
     console.log(err);
@@ -16,29 +15,30 @@ const getRecsService = async (medium) => {
 };
 
 export const getRecsByGenreService = async (medium) => {
-  const { genre_id } = medium;
+  const key = medium.moviedb_id || medium.goodReads_id;
+  const genre_id = medium.type === 'book' ? medium.book_genre.genreId : medium.genre_id[0];
   try {
     let movie, tv, book;
-    if (medium.type === "movie" || medium.type === "tv") {
-      movie = await axios.get(`http://localhost:80/api/rec/movies/genre/${genre_id[0]}`);
-      tv = await axios.get(`http://localhost:80/api/rec/tv/genre/${genre_id[0]}`);
-      book = await axios.get(`http://localhost:80/api/db/getBookRecsByGenre/${genre_id[0]}`);
-    } else if (medium.type === "book") {
-      movie = await axios.get(`http://localhost:80/api/rec/movies/genre/${medium.book_genre.genreId}`);
-      tv = await axios.get(`http://localhost:80/api/rec/tv/genre/${medium.book_genre.genreId}`);
-      book = await axios.get(`http://localhost:80/api/db/getBookRecsByGenre/${medium.book_genre.genreId}`);
-    }
+    movie = await axios.get(`http://localhost:80/api/rec/movies/genre/${genre_id}`);
+    tv = await axios.get(`http://localhost:80/api/rec/tv/genre/${genre_id}`);
+    book = await axios.get(`http://localhost:80/api/db/getBookRecsByGenre/${genre_id}`);
     let arr = [...movie.data, ...tv.data];
-    if (book !== null) {
-      arr.concat(book.data);
+    const data = {
+      movie: medium,
+      user: localStorage.id_token
     }
+    axios.post('http://localhost:80/api/db/addMedium', { data });
+    book.data.forEach(rec => {
+      if (rec) {
+        arr.push(rec);
+      }
+    })
+
+    axios.post('http://localhost:80/api/redis/caching', { key, value: arr });
     return arr;
   } catch (err) {
-    throw (err);
+    console.log(err);
   }
 }
 
 export default getRecsService
-// export { getRecsService, getRecsByGenreService };
-// export getRecsByGenreService;
-//`https://api.themoviedb.org/3/tv/${tv_id}/recommendations`;
