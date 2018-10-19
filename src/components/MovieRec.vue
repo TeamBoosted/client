92a549f2b7fb7a2564dd0b2b4510e0ee8f870745
Walker Griffin <38767282+WalkerJG33@users.noreply.github.com>
2018-10-19 12:49:26 -0500

    Refactor/flip thumbs (#83)
    * Thumbs up generates robust recommendations
    
    * Removed duplicates
    
    * did this work?
    
    * staging changes for updating
    
    * updated getThreeRec service
    
    * fixed adding to user on thumbs up
    
    * Thumbs moved
    
    * staging for changes
    
    * limiting and mixing recs
    
    * removed default variable
    
    * added shuffling for new users
    
    * flipped thumbs

--- src/components/MovieRec.vue
+++ src/components/MovieRec.vue
@@ -17,15 +17,15 @@
       <ul>
         <div class="flex-container">
           <div class="box">
-            <i class="material-icons" id="thumbs-up" @click="thumbsUp(currentMovie); getGenreRecs(currentMovie); increment();">thumb_up</i>
-          </div>
+            <i class="material-icons" name="thumbs-down" @click="thumbsDown(); increment();">thumb_down</i>
+          </div> 
           <div class="poster">
             <img :src="currentMovie.image" class="poster">
           </div>  
           <br>
           <br>
           <div class="box">
-            <i class="material-icons" name="thumbs-down" @click="thumbsDown(); increment();">thumb_down</i>
+            <i class="material-icons" id="thumbs-up" @click="thumbsUp(currentMovie); getGenreRecs(currentMovie); increment();">thumb_up</i>
           </div>
         </div>
         <li>{{currentMovie.synopsis}}</li>
